31d2f3ce7aa0c22e1b9c69cc513859fe74594cf5
rlAlan <momoriallan@gmail.com>
2024-07-05 10:57:51 +0100

    adding web project

---
+++ Models/ColourScheme.cs
@@ -0,0 +1,17 @@
+﻿// Properties for the Colour Scheme
+
+namespace BlazorAppTest.Models
+{
+	public class ColourScheme
+	{
+		public ColourScheme() { }
+		private string ContentColDefault = "black";
+		public string ContentCol { get { return ContentColDefault; } set { ContentColDefault = value; } }
+	
+		private string BackgroundDefault = "white";
+		public string Background { get { return BackgroundDefault; } set { BackgroundDefault = value; } }
+
+		private string AccentColDefault = "white";
+		public string AccentCol { get { return AccentColDefault; } set { AccentColDefault = value; } }
+	}
+}
---
+++ Models/Search.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAppTest.Models
+{
+	public class Search
+	{
+		public string SearchText { get { return SearchText; } set { SearchText = value; } }
+		public string StatusOfSearch{ get { return StatusOfSearch; } set { StatusOfSearch = value; } }
+	}
+}
